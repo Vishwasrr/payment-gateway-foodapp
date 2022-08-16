cf0bcc0904bf94dcbfa80e467536effadcf606ee
Vishwas R <48607495+Vishwasrr@users.noreply.github.com>
2022-08-16 18:25:36 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,15 @@
-import React,{Fragment,useContext} from 'react'
+import React, { Fragment, useContext } from 'react'
 import { FoodContext } from '../App'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Home() {
     let context = useContext(FoodContext)
+    console.log(context.data)
     return (
         <div className='home-wrapper'>
             {
-                context.data.map((e,i)=>{
-                    return<Fragment key={i}>
-                        <Link to={`/`+e.name.toLowerCase()} className='item-link'>
+                context.data.map((e, i) => {
+                    return <Fragment key={i}>
+                        <Link to={`/` + e.name.toLowerCase()} className='item-link'>
                             <div className="item-wrapper">
                                 <img src={e.image} className='item-image'></img>
                                 <div className="item-name">{e.name}</div>
