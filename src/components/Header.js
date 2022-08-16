56656a2f9c27bb1f420fec7638bb481c00faa1b7
Vishwas R <48607495+Vishwasrr@users.noreply.github.com>
2022-08-16 18:25:13 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React, { useContext } from "react";
 import { FoodContext } from "../App";
 import { Link } from "react-router-dom";
 
+
 function Header() {
   let context = useContext(FoodContext);
   return (
     <div className="head-wrapper">
       <div className="head-title">Food Ordering Portal</div>
+      
       <div className="head-cart">
         <Link to="cart">
           <i class="fa-solid fa-cart-shopping fa-xl"></i>
@@ -21,7 +23,9 @@ function Header() {
           <i class="fa-solid fa-house fa-xl"></i>
         </Link>
       </div>
+
     </div>
+
   );
 }
 
