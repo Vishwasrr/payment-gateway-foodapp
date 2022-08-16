e3b9da381deed9b12db491ad51194ae7068863fa
Vishwas R <48607495+Vishwasrr@users.noreply.github.com>
2022-08-16 18:27:06 +0530

    Update Pizza.js

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,38 +1,38 @@
-import React,{useContext,useEffect,useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { FoodContext } from '../App'
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 function Pizza() {
     let context = useContext(FoodContext)
-    let [products,setProducts]=useState([]);
+    let [products, setProducts] = useState([]);
     let navigate = useNavigate();
-    let getData = ()=>{
-        if(context.data.length>0)
+    let getData = () => {
+        if (context.data.length > 0)
             setProducts(context.data[0].subItemsData.subItems)
         else
             navigate('/')
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getData();
-    },[])
+    }, [])
     return (
         <div className='product-wrapper'>
             <h2>Tasty Pizzas</h2>
             {
-                products.map((e,i)=>{
+                products.map((e, i) => {
                     return <div className='product-item-wrapper' key={i}>
                         <div className='product-details'>
                             <h4>{e.name}</h4>
                             <div className='product-price'> &#x20B9; {e.price} </div>
                             <div className='product-description'>{e.description}</div>
-                            <button className='product-btn' onClick={()=>{
+                            <button className='product-btn' onClick={() => {
                                 context.cart.push(e);
                                 context.setCartValue(context.cart.length)
                             }}
-                            
-                            >Order Now</button>
+
+                            >Add to Cart</button>
                         </div>
                         <div className='product-image'>
                             <img src={e.image} alt={e.name}></img>
